065b92c7e250933c60a5b526ec9a471f8a125d04
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-10-15 11:46:28 +0530

    All routes and controller basic structure is done

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+const tasks = require("./routes/tasks");
+
+app.use("/api/v1/tasks", tasks);
+
+// app.get('/api/v1/tasks')         - get all the tasks
+// app.post('/api/v1/tasks')        - create a new task
+// app.get('/api/v1/tasks/:id')     - get single task
+// app.patch('/api/v1/tasks/:id')   - update task
+// app.delete('/api/v1/tasks/:id')  - delete task
+
+const port = 8000;
+app.listen(port, console.log(`Server is up and running at ${port}`));
